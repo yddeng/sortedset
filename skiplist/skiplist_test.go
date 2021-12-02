9f4606f97a5b9de09b444448ef91366b547b9d60
yidongdeng <248244142@qq.com>
2021-12-02 17:59:43 +0800

    skiplist

---
+++ skiplist/skiplist_test.go
@@ -0,0 +1,59 @@
+package skiplist
+
+import (
+	"fmt"
+	"testing"
+)
+
+type User struct {
+	score float64
+	id    string
+}
+
+func (u *User) Less(other interface{}) bool {
+	if u.score > other.(*User).score {
+		return true
+	}
+	if u.score == other.(*User).score && len(u.id) > len(other.(*User).id) {
+		return true
+	}
+	return false
+}
+
+func TestNew(t *testing.T) {
+	us := make([]*User, 7)
+	us[0] = &User{6.6, "hi"}
+	us[1] = &User{4.4, "hello"}
+	us[2] = &User{2.2, "world"}
+	us[3] = &User{3.3, "go"}
+	us[4] = &User{1.1, "skip"}
+	us[5] = &User{2.2, "list"}
+	us[6] = &User{3.3, "lang"}
+
+	// insert
+	sl := New()
+	for i := 0; i < len(us); i++ {
+		sl.Insert(us[i])
+	}
+
+	// traverse
+	for e := sl.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value.(*User).id, "-->", e.Value.(*User).score)
+	}
+	t.Log(sl.Len())
+
+	// rank
+	rank1 := sl.GetRank(&User{2.2, "list"})
+	rank2 := sl.GetRank(&User{6.6, "hi"})
+	if rank1 != 6 || rank2 != 1 {
+		t.Fatal()
+	}
+	if e := sl.GetElementByRank(2); e.Value.(*User).score != 4.4 || e.Value.(*User).id != "hello" {
+		t.Fatal(e)
+	}
+
+	nu := &User{score: 0, id: "dfsf"}
+	t.Log(sl.Find(nu), sl.GetRank(nu))
+
+	t.Log(sl.GetRank(us[0]), sl.GetElementByRank(sl.Len()).Value)
+}
